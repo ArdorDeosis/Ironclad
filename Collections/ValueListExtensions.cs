3b67a46311dadcabb5666fea0f452e15c1f4d7a4
Theobald Beyer <theobald.beyer@t-online.de>
2023-04-20 08:53:11 +0200

    initial commit

---
+++ Collections/ValueListExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Ironclad.Collections;
+
+/// <summary>
+/// Extension methods for <see cref="ValueList{T}"/>.
+/// </summary>
+public static class ValueListExtensions
+{
+  /// <summary>
+  /// Converts an enumerable to a <see cref="ValueList{T}"/>.
+  /// </summary>
+  public static ValueList<T> AsValueList<T>(this IEnumerable<T> enumerable) => new(enumerable.ToArray());
+}

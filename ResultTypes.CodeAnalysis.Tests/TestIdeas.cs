ba7f69c4b2539a58798888b3301f0b89d1e03e0c
Theobald Beyer <theobald.beyer@in-tech.com>
2023-05-07 11:37:02 +0200

    added missing files

---
+++ ResultTypes.CodeAnalysis.Tests/TestIdeas.cs
@@ -0,0 +1,127 @@
+using Ironclad.ResultTypes;
+
+namespace ResultTypes.CodeAnalysis.Tests;
+
+[TestFixture]
+public class TestIdeas
+{
+  private readonly Result<int, string> result = null!;
+  
+  // NOTES:
+  //
+  // ways to determine the state of a result:
+  // - implicit conversion
+  // - OrThrow()
+  // - IsError(), IsError(out TError)
+  // - assigning .Success or .Error
+  // - assigning via implicit conversion
+  //
+  // invalid operations in error state:
+  // - implicit conversion in error state
+  // - using out error value in success state
+  // redundant operations in any state:
+  // - Or(), OrDefault(), OrThrow()
+  // - IsError(), IsError(out TError)
+  
+  [Test]
+  public void ImplicitConversion_NoCheck_ShouldWarnAboutMissingCheck()
+  {
+    var value1 = 0 + Result<int, string>.Success(6);
+    var value2 = 0 + (Result<int, string>)6;
+    var value3 = 0 + result;
+    object obj = result;
+  }
+  
+  [Test]
+  public void ImplicitConversion_UsingOr_ShouldNotWarnAboutMissingCheck()
+  {
+    var value1 = 0 + result.OrDefault;
+    var value2 = 0 + result.Or(default);
+  }
+  
+  [Test]
+  public void ImplicitConversion_InsideCheck_ShouldNotWarnAboutMissingCheck()
+  {
+    // TODO: use error returning value, too
+    if (result.IsError())
+    {
+      var value = 0 + result;
+    }
+  }
+  
+  [Test]
+  public void ImplicitConversion_AfterEarlyReturn_ShouldNotWarnAboutMissingCheck()
+  {
+    // TODO: test throw, too
+    if (result.IsError())
+      return;
+    var value = 0 + result;
+  }
+  
+  [Test]
+  public void UsingErrorValue_AfterEarlyReturn_ShouldWarnAboutInvalidErrorValue()
+  {
+    var local = Result<int, int>.Success(432);
+    if (local.IsError(out var error))
+      return;
+    var value = "" + error;
+  }
+  
+  [Test]
+  public void UsingErrorValue_InsideSuccessCase_ShouldWarnAboutInvalidErrorValue()
+  {
+    if (!result.IsError(out var error))
+    {
+      var value = "" + error;
+    }
+  }
+  
+  [Test]
+  public void CheckingForError_AfterOrThrow_ShouldWarnAboutObsoleteCheck()
+  {
+    var value = result.OrThrow(new Exception());
+    // TODO: also check IsError without out parameter and negated version
+    if (result.IsError(out var error))
+    {
+      
+    }
+  }
+  
+  [Test]
+  public void UsingAnyOr_AfterOrThrow_ShouldWarnAboutObsoleteCall()
+  {
+    var value = result.OrThrow(new Exception());
+    
+    // should all warn about obsolete call
+    value = result.Or(0);
+    value = result.OrDefault;
+    value = result.OrThrow(default!);
+  }
+  
+  [Test]
+  public void UsingAnyOr_AfterErrorCheck_ShouldWarnAboutObsoleteCall()
+  {
+    var value = 0;
+    
+    // both cases should warn (e.g. 'fallback not needed' vs. 'always fallback')
+    if(result.IsError())
+    {
+      value = result.Or(0);
+      value = result.OrDefault;
+      value = result.OrThrow(default!);
+    }
+    else
+    {
+      value = result.Or(0);
+      value = result.OrDefault;
+      value = result.OrThrow(default!);
+    }
+
+    if (result.IsError())
+    {
+      var x = 4;
+    }
+  }
+  
+  // TODO: check that reassignments reset the analysis state
+}
---
+++ ResultTypes.Playground/Playground.cs
@@ -0,0 +1,82 @@
+﻿using Ironclad.ResultTypes;
+
+namespace ResultTypes.Playground;
+
+public class DummyData { public object Value { get; } }
+
+
+public class Playground
+{
+  private readonly int? nullableValue = default!;
+  private readonly DummyData? nullableReference = default!;
+  
+  private readonly Result<int, string> valueResult = null!;
+  private readonly Result<int?, string> nullableValueResult = null!;
+  private readonly Result<DummyData, string> referenceResult = null!;
+  private readonly Result<DummyData?, string> nullableReferenceResult = null!;
+  
+  public void OrThrow_ValueNullabilityIsCorrect()
+  {
+    // should be of type int
+    var value1 = valueResult.OrThrow(new Exception());
+    
+    // should be of type int?
+    var value2 = nullableValueResult.OrThrow(new Exception());
+    
+    // should be of type DummyData
+    var value3 = referenceResult.OrThrow(new Exception());
+    
+    // should be of type DummyData?
+    var value4 = nullableReferenceResult.OrThrow(new Exception());
+  }
+  
+  public void OrDefault_ValueNullabilityIsCorrect()
+  {
+    // should be of type int
+    var value1 = valueResult.OrDefault;
+    
+    // should be of type int?
+    var value2 = nullableValueResult.OrDefault;
+    
+    // both should be of type DummyData?
+    var value3 = referenceResult.OrDefault;
+    var value4 = nullableReferenceResult.OrDefault;
+  }
+  
+  public void ValueResult_Or_ValueNullabilityIsCorrect()
+  {
+    // all should be of type int
+    var value1 = valueResult.Or(5);
+    
+    // should not compile
+    // var value2 = valueResult.Or(nullableValue);
+    // var value3 = valueResult.Or(null);
+  }
+  
+  public void NullableValueResult_Or_ValueNullabilityIsCorrect()
+  {
+    // all should be of type int?
+    // no warnings
+    var value1 = nullableValueResult.Or(5);
+    var value2 = nullableValueResult.Or(nullableValue);
+    var value3 = nullableValueResult.Or(null);
+    
+  }
+  public void ReferenceResult_Or_ValueNullabilityIsCorrect()
+  {
+    // all should be of type DummyData
+    // no warnings
+    var value1 = referenceResult.Or(new DummyData());
+    // warnings
+    var value2 = referenceResult.Or(nullableReference);
+    var value3 = referenceResult.Or(null);
+  }
+  
+  public void NullableReferenceResult_Or_ValueNullabilityIsCorrect()
+  {
+    // all should be of type DummyData?
+    var value1 = nullableReferenceResult.Or(new DummyData());
+    var value2 = nullableReferenceResult.Or(nullableReference);
+    var value3 = nullableReferenceResult.Or(null);
+  }
+}

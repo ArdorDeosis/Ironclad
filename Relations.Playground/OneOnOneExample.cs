fa3e55482f3c8c63e830728ae79ea0beee95ee77
Theo Beyer (in-tech) <theobald.beyer.ext@asmpt.com>
2023-05-09 16:53:48 +0200

    added thought experiemnt about relations

---
+++ Relations.Playground/OneOnOneExample.cs
@@ -0,0 +1,36 @@
+namespace Playground;
+
+public class MonogamousPerson
+{
+	// instance needs to be injected
+	private readonly UndirectedRelationshipManager<MonogamousPerson> relationshipManager;
+
+	public MonogamousPerson? Partner
+	{
+		get => relationshipManager.Get(this);
+		set => relationshipManager.Set(this, value);
+	}
+}
+
+public class UndirectedRelationshipManager<T> where T : class
+{
+	private readonly Dictionary<T, T> dictionary = new();
+	
+	public T? Get(T self) => dictionary.TryGetValue(self, out var value) ? value : null;
+	
+	public void Set(T self, T? value)
+	{
+		if (dictionary.ContainsKey(self))
+			throw new InvalidOperationException();
+		if (value is null)
+		{
+			dictionary.Remove(dictionary[self]);
+			dictionary.Remove(self);
+			return;
+		}
+		if (dictionary.ContainsKey(value))
+			throw new InvalidOperationException();
+		dictionary.Add(self, value);
+		dictionary.Add(value, self);
+	}
+}

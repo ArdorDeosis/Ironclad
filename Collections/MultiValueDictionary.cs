f5a859bbe0045887568d8b2b1d145ca3462e4c0b
Theo Beyer (in-tech) <theobald.beyer.ext@asmpt.com>
2023-05-15 17:23:25 +0200

    more ideas for collections and relations

---
+++ Collections/MultiValueDictionary.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Immutable;
+
+namespace Ironclad.Collections;
+
+// TODO: add equality comparers
+public sealed class MultiValueDictionary<TKey, TValue> where TKey : notnull
+{
+	private readonly Dictionary<TKey, HashSet<TValue>> dictionary = new();
+
+	public IReadOnlyCollection<TValue> this[TKey key] => dictionary.TryGetValue(key, out var set)
+		? set
+		: Array.Empty<TValue>();
+
+	public bool Add(TKey key, TValue value)
+	{
+		if (!dictionary.ContainsKey(key))
+			dictionary.Add(key, new HashSet<TValue>());
+		return dictionary[key].Add(value);
+	}
+
+	public void Clear() => dictionary.Clear();
+
+	public sealed class ValueSet<TKey, TValue> : IReadOnlyCollection<TValue> where TKey : notnull
+	{
+		private readonly MultiValueDictionary<TKey, TValue> parent;
+		private readonly TKey key;
+
+		internal ValueSet(MultiValueDictionary<TKey, TValue> parent, TKey key)
+		{
+			this.parent = parent;
+			this.key = key;
+		}
+
+		public IEnumerator<TValue> GetEnumerator() => parent.dictionary.ContainsKey(key)
+			? parent[key].GetEnumerator()
+			: new EmptyEnumerator<TValue>();
+
+		IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+		public bool Add(TValue item) => set.Add(item);
+
+		public void Clear()
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool Contains(T item)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void CopyTo(T[] array, int arrayIndex)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool Remove(T item)
+		{
+			throw new NotImplementedException();
+		}
+
+		public int Count { get; }
+		public bool IsReadOnly { get; }
+	}
+}
--- Relations.Playground/ManyOnManyExample.cs
+++ Relations.Playground/ManyOnManyExample.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Data;
 
 namespace Playground;
 
@@ -10,11 +11,13 @@ public class Person
 public class UndirectedMultiRelationshipManager<T> where T : class
 {
 	private readonly Dictionary<T, HashSet<T>> connections = new();
-	
-	public ICollection<T> Get(T self) => connections.TryGetValue(self, out var value) ? value : Array.Empty<T>();
+
+	private HashSet<T> this[T item] => connections.ContainsKey(item) ? connections[item] : new HashSet<T>();
+
+	public ICollection<T> Get(T self) => new CollectionHandle<T>(this, self);
 	
 #pragma warning disable CS0693 // type parameter has same name
-	internal class CollectionHandle<T> : ICollection<T> where T : class
+	public class CollectionHandle<T> : ICollection<T> where T : class
 #pragma warning restore CS0693
 	{
 		private readonly UndirectedMultiRelationshipManager<T> manager;
@@ -30,34 +33,36 @@ public class UndirectedMultiRelationshipManager<T> where T : class
 			? manager.connections[self].GetEnumerator()
 			: new HashSet<T>.Enumerator();
 
-		IEnumerator IEnumerable.GetEnumerator()
-		{
-			return GetEnumerator();
-		}
+		IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
 		public void Add(T item)
 		{
+			if (manager.connections.TryGetValue(self, out var connections) && connections.Contains(item))
+				throw new InvalidOperationException("self and item are already connected");
 			if (!manager.connections.ContainsKey(self)) 
 				manager.connections.Add(self, new HashSet<T>());
-			manager.connections[self].Add(item);
 			if (!manager.connections.ContainsKey(item)) 
 				manager.connections.Add(item, new HashSet<T>());
-			manager.connections[self].Add(self);
+			manager.connections[self].Add(item);
+			manager.connections[item].Add(self);
 		}
 
 		public void Clear()
 		{
-			throw new NotImplementedException();
+			foreach (var VARIABLE in manager.connections.ContainsKey(self))
+			{
+				throw new NotImplementedException();
+			}
 		}
 
-		public bool Contains(T item)
-		{
-			throw new NotImplementedException();
-		}
+		public bool Contains(T item) => manager.connections.ContainsKey(item);
 
 		public void CopyTo(T[] array, int arrayIndex)
 		{
-			throw new NotImplementedException();
+			if (manager.connections.TryGetValue(self, out var connections))
+				connections.CopyTo(array, arrayIndex);
+			else
+				Array.Empty<T>().CopyTo(array, arrayIndex);
 		}
 
 		public bool Remove(T item)
